086086833de1e5f6f671ed821fe23261d5c2bab2
Adauto Moisés <adautomaleandro@gmail.com>
2024-07-26 20:26:55 -0300

    DELEGANDO COMPORTAMENTO COM COMPOSIÇÃO p.2

--- Design_Patterns_Java/cap_03/Main.java
+++ Design_Patterns_Java/cap_03/Main.java
@@ -21,3 +21,14 @@ public class Main {
 
 
 // Padrão State
+// Basicamente só mantem a lógica na entidade e cada subclasse da interface terá seu comportamento
+// especializado, permitindo alterar o comportamento da entidade só trocando a instância,
+// alterando seu estado.
+
+
+// Padrão Observer
+// No padrão observer, temos classes que são observadas e classes que observam, resumidamente.
+// Todos os observadores de uma classe são notificados quando ocorre alguma alteração na classe
+// que observam. Existem muitos pontos a serem considerados, por exemplo, cada observador pode
+// ser notificado de uma thread diferente, ou se um tiver exceção, o que ocorre com os outros?
+// Também podemos considerar ter notificações diferentes para cada alteração...
